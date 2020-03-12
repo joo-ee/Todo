883dcbd8608b93e1e716d40fed2d996d54bea500
joohee <vkdlfl0224@naver.com>
2020-03-12 21:29:46 +0900

    [#2] set TodoItemTemplate

--- src/Todo/Todo-item.js
+++ src/Todo/Todo-item.js
@@ -2,18 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TodoItemTemplate = styled.div`
-    padding: 10px;
-    float:left;
+    padding: 1rem;
+    align-items: center; /*세로 가운데 정렬*/
+
+    :hover {
+        background: #F0F0F0;
+    }
 `
 const UpdateButton = styled.button`
     border: none;
     background: none;
     color: #498EAF;
+
+    padding-left: 14rem; /*이렇게 조절하는 게 맞는 건가*/
+    
 `
 const DeleteButton = styled.button`
     border: none;
     background: none;
     color: #631F16;
+
+
 `
 
 export default function TodoItem() {
